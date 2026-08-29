5c3764aa7f4034a1f2eb1df29bfc985e9fa1ab47
deep1283 <deepmishra1283@gmail.com>
2026-08-29 23:37:39 +0530

    fix(login): restore guest entry label

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -300,7 +300,7 @@ export function LoginForm() {
                 d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
               />
             </svg>
-            Sign in as Guest
+            Continue as guest
           </Link>
         </GlassCardFooter>
       </GlassCard>
